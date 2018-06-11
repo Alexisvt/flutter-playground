5c62fc57e87a5be70d77c71e0e826a3f131e625f
Alexis Villegas Torres <alexisvt@gmail.com>
2018-06-10 23:02:02 -0600

    finished sample

---
+++ raise_btn_params_sample/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _value = 'Hello world';
+
+  _onPressed(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value),
+              new RaisedButton(
+                onPressed: () => this._onPressed('Hello Alexis'),
+                child: new Text('Clicked me'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
