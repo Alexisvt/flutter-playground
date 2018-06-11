ee83a0912d0e4fe611cdc456f0b1cba9b6bfbfcd
Alexis Villegas Torres <alexisvt@gmail.com>
2018-06-10 21:15:06 -0600

    finished sample

--- appbar_sample/lib/main.dart
+++ appbar_sample/lib/main.dart
@@ -12,17 +12,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _value = 0;
+
+  void _add() => setState(() => _value++);
+  void _remove() => setState(() => _value--);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Name here'),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.add),
+            onPressed: _add,
+          ),
+          new IconButton(
+            icon: new Icon(Icons.remove),
+            onPressed: _remove,
+          )
+        ],
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
-            children: <Widget>[new Text('Add Widgets here')],
+            children: <Widget>[
+              new Text(
+                _value.toString(),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 37.0),
+              )
+            ],
           ),
         ),
       ),
