262b4b499c6d2aba93a91c4f1326121992a1a4ec
Alexis Villegas Torres <alexisvt@gmail.com>
2018-06-10 22:42:10 -0600

    finished sample

---
+++ date_picker/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+main(List<String> args) {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _value = '';
+
+  Future _selectDate() async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2019));
+
+    if (picked != null) {
+      setState(() {
+        _value = picked.toString();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value),
+              new RaisedButton(
+                onPressed: _selectDate,
+                child: new Text('Click me'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
