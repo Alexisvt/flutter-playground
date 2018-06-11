e90be353ff325a29c81c58b263fe71751312f874
Alexis Villegas Torres <alexisvt@gmail.com>
2018-06-10 20:48:00 -0600

    initial commit

---
+++ appbar_sample/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[new Text('Add Widgets here')],
+          ),
+        ),
+      ),
+    );
+  }
+}
