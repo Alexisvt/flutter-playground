aed9e4eacc001ead19e31c21f950b3588f98ffe6
Alexis <alexisvt@gmail.com>
2018-11-22 13:49:09 -0600

    adding a new page, the AuthPage widget, and refactor the HomePage for ProductsPage

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './pages/home.dart';
+import './pages/auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
           accentColor: Colors.deepPurple),
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
