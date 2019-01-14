b6bf84c5520342922070cb5fe0da123b28734307
Alexis Villegas Torres <alexisvt@gmail.com>
2019-01-14 15:05:05 -0600

    adding style to some pages 💄💅🏻

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -52,6 +52,8 @@ class _MyAppState extends State<MyApp> {
             builder: (BuildContext context) => ProductPage(
                   _products[index]['title'],
                   _products[index]['image'],
+                  _products[index]['price'],
+                  _products[index]['description'],
                 ),
           );
         }
