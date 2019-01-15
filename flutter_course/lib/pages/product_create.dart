ab973891b26a6fbf09969782d7248b98091ba1c7
Alexis Villegas Torres <alexisvt@gmail.com>
2019-01-15 14:25:35 -0600

    adding media queries to product create page

--- flutter_course/lib/pages/product_create.dart
+++ flutter_course/lib/pages/product_create.dart
@@ -16,9 +16,14 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
+    final double targetPadding = deviceWidth - targetWidth;
+
     return Container(
       margin: EdgeInsets.all(10.0),
       child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
         children: <Widget>[
           _buildTitleTextField(),
           _buildDescriptionTextField(),
@@ -59,7 +64,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   }
 
   Widget _buildDescriptionTextField() {
-    return TextField( 
+    return TextField(
       decoration: InputDecoration(labelText: 'Product description'),
       keyboardType: TextInputType.multiline,
       maxLines: 4,
