4b390f6720866a30230462e7d6c3720fb4ca40de
Alexis Villegas Torres <alexisvt@gmail.com>
2019-01-17 21:42:12 -0600

    connected the user model to auth page

--- flutter_course/lib/pages/auth.dart
+++ flutter_course/lib/pages/auth.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import '../scoped-models/main.dart';
 
 class AuthPage extends StatefulWidget {
   @override
@@ -41,10 +43,15 @@ class _AuthPageState extends State<AuthPage> {
                     _buildPasswordTextField(),
                     _buildAcceptSwitch(),
                     SizedBox(height: 20.0),
-                    RaisedButton(
-                      textColor: Colors.white,
-                      child: Text('LOGIN'),
-                      onPressed: _submitForm,
+                    ScopedModelDescendant<MainModel>(
+                      builder: (BuildContext context, Widget child,
+                          MainModel model) {
+                        return RaisedButton(
+                          textColor: Colors.white,
+                          child: Text('LOGIN'),
+                          onPressed: () => _submitForm(model.login),
+                        );
+                      },
                     ),
                   ],
                 ),
@@ -119,14 +126,13 @@ class _AuthPageState extends State<AuthPage> {
     );
   }
 
-  void _submitForm() {
+  void _submitForm(Function login) {
     if (!_formKey.currentState.validate() || !_formData['acceptTerms']) {
       return;
     }
     _formKey.currentState.save();
 
-    print(_formData['email']);
-    print(_formData['password']);
+    login(_formData['email'], _formData['password']);
     Navigator.pushReplacementNamed(context, '/products');
   }
 }
