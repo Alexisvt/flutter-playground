bf8ee2180315fee90db6a90008550838c9336fed
Alexis Villegas Torres <alexisvt@gmail.com>
2019-02-17 00:47:21 -0600

    adding icons to both android and ios

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -64,6 +64,7 @@ class _MyAppState extends State<MyApp> {
     return ScopedModel<MainModel>(
       model: _model,
       child: MaterialApp(
+        title: 'EasyList',
         theme: getAdaptiveThemeData(context),
         routes: {
           '/': (BuildContext context) =>
