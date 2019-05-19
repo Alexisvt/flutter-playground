ea6c7c3daf90148529376f651f41edff15500151
Alexis Villegas Torres <alexisvt@gmail.com>
2019-05-19 15:38:32 -0600

    remove maximilliam course to a separete repo

--- flutter_course/lib/models/location_data.dart
+++
@@ -1,7 +0,0 @@
-class LocationData {
-  final double latitude;
-  final double longitude;
-  final String address;
-
-  LocationData({this.latitude, this.longitude, this.address});
-}
--- flutter_course/lib/shared/global_config.dart
+++
@@ -1,1 +0,0 @@
-final String apiKey = 'AIzaSyAgKswUsxTK6E-_9wA6mA8vU5_i8LfY29s';
